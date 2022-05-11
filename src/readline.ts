53b041f5728fcbe942dc507edeb62302b9025fb6
KazuhiroIto0127 <kazuhiroito0127@gmail.com>
2022-05-12 01:14:41 +0900

    update readline.ts

--- src/readline.ts
+++ src/readline.ts
@@ -5,7 +5,18 @@ const rl = createInterface({
     output: process.stdout
 });
 
-rl.question('文字列を入力してね:', (line) => {
-    console.log(`${line}が入力されました。`)
+// rl.question('文字列を入力してね:', (line) => {
+//     console.log(`${line}が入力されました。`)
+//     rl.close();
+// })
+
+rl.question('数字を入力してね:', (line) => {
+    const input = Number(line);
+    console.log(`${input + 1000}`)
     rl.close();
 })
+
+
+// runtime error
+// const bigint = BigInt("foooo");
+// console.log(`${bigint}`);
