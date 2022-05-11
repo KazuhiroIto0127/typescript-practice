8c14f1ef3514662569efb328f6944dd4c6682f14
KazuhiroIto0127 <kazuhiroito0127@gmail.com>
2022-05-12 01:00:17 +0900

    update index.ts. and create readline.ts

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,28 @@
-const message: string = 'Hello World';
-console.log(message);
+const message: string = 'Hello';
+const target: string = 'World';
+console.log(message + target);
+
+
+/***********
+/ Number型
+*************/
+// 53bitに収まらないため正確に表示できない
+console.log(9007199254740993);
+
+// おかしな結果になる。なぜか0.30000000000..4
+console.log(0.1 + 0.2);
+
+/***********
+/ BigInt型
+*************/
+const bignum: bigint = (123n + 456n);
+console.log(bignum);
+
+
+/***********
+/ テンプレートリテラル
+*************/
+const str1 = `Hello
+world!!!!!!!!
+`;
+console.log(`${str1}`);
